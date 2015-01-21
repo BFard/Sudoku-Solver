06f8cf696e3a85e9d58069142833a39379c3f77c
BFard <bijanfard42@gmail.com>
2015-01-20 20:27:30 -0800

    Updated JS
    Faster

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-var validate = function() {
-    for(i = 0; i < 81; i++) {
+var board = [];
+var getBoard = function() {
+    board = [];
+    for (i = 0; i < 81; i++) {
         var cell = $("input").eq(i).val();
         var accepted = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
         if (accepted.indexOf(cell) === -1) {
             return false;
         }
+        else if (cell === "") {
+            board.push(0);
+        }
+        else {
+            board.push(parseInt(cell));
+        }
     }
-    for (i = 1; i < 10; i++) {
-        var row = "#" + i.toString();
+}
+var validate = function() {
+    if (board.length != 81) {
+        return false;
+    }
+    for (i = 0; i < 9; i++) {
         var rows = [];
-        for (j = 0; j < 9; j++) {
-            rows.push($(row).find("input").eq(j).val());
-            if (rows.length === 9) {
-                var checked = [];
-                for (g = 0; g < 9; g++) {
-                    var check = rows[g];
-                    checked.push(check);
-                    if (check != "" && checked.indexOf(check) != checked.lastIndexOf(check)) {
-                        return false;
-                    }
+        var first = i * 9;
+        for (j = 1; j < 10; j++) {
+            var temp = [];
+            temp.push(board.slice(first + (j - 1), first + j));
+            rows.push(temp[0][0]);
+        }
+        if (rows.length === 9) {
+            var checked = [];
+            for (g = 0; g < 9; g++) {
+                var check = rows[g];
+                checked.push(check);
+                if (check != 0 && checked.indexOf(check) != checked.lastIndexOf(check)) {
+                    return false;
                 }
             }
         }
@@ -26,83 +41,119 @@ var validate = function() {
     for (i = 0; i < 9; i++) {
         var column = [];
         var checked = [];
-        for (j = 1; j < 10; j++) {
-            var row = "#" + j.toString();
-            column.push($(row).find("input").eq(i).val());
+        for (j = 0; j < 9; j++) {
+            var first = j * 9;
+            var temp = [];
+            temp.push(board.slice(first + i, first + i + 1));
+            column.push(temp[0][0]);
         }
         for (g = 0; g < 9; g++) {
             var check = column[g];
             checked.push(check);
-            if (check != "" && checked.indexOf(check) != checked.lastIndexOf(check)) {
+            if (check != 0 && checked.indexOf(check) != checked.lastIndexOf(check)) {
                 return false;
             }
         }
     }
     for (i = 0; i < 9; i++) {
         var three = [];
-        for (j = 0; j < 3; j++) {
-            switch (i) {
-                case 0:
-                    for (g = 1; g < 4; g++) {
-                    var row = "#" + g.toString();
-                    three.push($(row).find("input").eq(j).val());
-                    }
-                    break;
-                case 1:
-                    for (g = 1; g < 4; g++) {
-                    var row = "#" + g.toString();
-                    three.push($(row).find("input").eq(j + 3).val());
-                    }
-                    break;
-                case 2:
-                    for (g = 1; g < 4; g++) {
-                    var row = "#" + g.toString();
-                    three.push($(row).find("input").eq(j + 6).val());
-                    }
-                    break;
-                case 3:
-                    for (g = 4; g < 7; g++) {
-                    var row = "#" + g.toString();
-                    three.push($(row).find("input").eq(j).val());
-                    }
-                    break;
-                case 4:
-                    for (g = 4; g < 7; g++) {
-                    var row = "#" + g.toString();
-                    three.push($(row).find("input").eq(j + 3).val());
-                    }
-                    break;
-                case 5:
-                    for (g = 4; g < 7; g++) {
-                    var row = "#" + g.toString();
-                    three.push($(row).find("input").eq(j + 6).val());
-                    }
-                    break;
-                case 6:
-                    for (g = 7; g < 10; g++) {
-                    var row = "#" + g.toString();
-                    three.push($(row).find("input").eq(j).val());
-                    }
-                    break;
-                case 7:
-                    for (g = 7; g < 10; g++) {
-                    var row = "#" + g.toString();
-                    three.push($(row).find("input").eq(j + 3).val());
-                    }
-                    break;
-                case 8:
-                    for (g = 7; g < 10; g++) {
-                    var row = "#" + g.toString();
-                    three.push($(row).find("input").eq(j + 6).val());
-                    }
-                    break;
-            }
+        switch (i) {
+            case 0:
+                for (g = 0; g < 3; g++) {
+                    var first = g * 9;
+                    var temp = [];
+                    temp.push(board.slice(first, first + 3));
+                    three.push(temp[0][0]);
+                    three.push(temp[0][1]);
+                    three.push(temp[0][2]);
+                }
+                break;
+            case 1:
+                for (g = 0; g < 3; g++) {
+                    var first = g * 9;
+                    var temp = [];
+                    temp.push(board.slice(first + 3, first + 6));
+                    three.push(temp[0][0]);
+                    three.push(temp[0][1]);
+                    three.push(temp[0][2]);
+                }
+                break;
+            case 2:
+                for (g = 0; g < 3; g++) {
+                    var first = g * 9;
+                    var temp = [];
+                    temp.push(board.slice(first + 6, first + 9));
+                    three.push(temp[0][0]);
+                    three.push(temp[0][1]);
+                    three.push(temp[0][2]);
+                }
+                break;
+            case 3:
+                for (g = 3; g < 6; g++) {
+                    var first = g * 9;
+                    var temp = [];
+                    temp.push(board.slice(first, first + 3));
+                    three.push(temp[0][0]);
+                    three.push(temp[0][1]);
+                    three.push(temp[0][2]);
+                }
+                break;
+            case 4:
+                for (g = 3; g < 6; g++) {
+                    var first = g * 9;
+                    var temp = [];
+                    temp.push(board.slice(first + 3, first + 6));
+                    three.push(temp[0][0]);
+                    three.push(temp[0][1]);
+                    three.push(temp[0][2]);
+                }
+                break;
+            case 5:
+                for (g = 3; g < 6; g++) {
+                    var first = g * 9;
+                    var temp = [];
+                    temp.push(board.slice(first + 6, first + 9));
+                    three.push(temp[0][0]);
+                    three.push(temp[0][1]);
+                    three.push(temp[0][2]);
+                }
+                break;
+            case 6:
+                for (g = 6; g < 9; g++) {
+                    var first = g * 9;
+                    var temp = [];
+                    temp.push(board.slice(first, first + 3));
+                    three.push(temp[0][0]);
+                    three.push(temp[0][1]);
+                    three.push(temp[0][2]);
+                }
+                break;
+            case 7:
+                for (g = 6; g < 9; g++) {
+                    var first = g * 9;
+                    var temp = [];
+                    temp.push(board.slice(first + 3, first + 6));
+                    three.push(temp[0][0]);
+                    three.push(temp[0][1]);
+                    three.push(temp[0][2]);
+                }
+                break;
+            case 8:
+                for (g = 6; g < 9; g++) {
+                    var first = g * 9;
+                    var temp = [];
+                    temp.push(board.slice(first + 6, first + 9));
+                    three.push(temp[0][0]);
+                    three.push(temp[0][1]);
+                    three.push(temp[0][2]);
+                }
+                break;
         }
         var checked = [];
         for (j = 0; j < 9; j++) {
             var check = three[j];
             checked.push(check);
-            if (check != "" && checked.indexOf(check) != checked.lastIndexOf(check)) {
+            if (check != 0 && checked.indexOf(check) != checked.lastIndexOf(check)) {
                 return false;
             }
         }
@@ -113,18 +164,15 @@ var solve = function() {
     if (validate() === true) {
         var empty = [];
         for(pos = 0; pos < 81; pos++) {
-            if ($("input").eq(pos).val() === "") {
+            if (board[pos] === 0) {
                 empty.push(pos);
             }
         }
         for (cell = 0; cell < empty.length;) {
-            var current = $("input").eq(empty[cell]);
-            if (current.val() === "") {
-                current.val("0");
-            }
-            var value = parseInt(current.val()) + 1;
+            var current = empty[cell];
+            var value = board[current] + 1;
             var found = false;
-            current.val(value.toString());
+            board[current] = value;
             while (found != true && value < 10)  {
                 if (validate() === true) {
                     found = true;
@@ -132,13 +180,17 @@ var solve = function() {
                 }
                 else {
                     value++;
-                    current.val(value.toString());
+                    board[current] = value;
                 }
             }
             if (found != true) {
-                current.val("");
+                board[current] = 0;
                 cell--;
             }
+        }
+        for (i = 0; i < 81; i++) {
+            var fill = $("input").eq(i);
+            fill.val(board[i]);
         }
     }
     else {
@@ -176,6 +228,7 @@ var main = function() {
     });
     $("#load").click(load);
     $("#validate").click(function() {
+        getBoard();
         if (validate() === true) {
             $("#board").css("border", "10px solid #006400");
         }
@@ -187,6 +240,7 @@ var main = function() {
         }, 1500);
     });
     $("#solve").click(function() {
+        getBoard();
         if (solve() === false) {
             $("#board").css("border", "10px solid #990000");
         }
